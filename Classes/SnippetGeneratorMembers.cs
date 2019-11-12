629ef60a4ca4a56203a2d7fa9b10b36e151b5615
Amol Kobal <amol_kobal@rediffmail.com>
2019-11-12 16:02:52 +0530

    Generator Memebers

---
+++ Classes/SnippetGeneratorMembers.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SnippetGenerator.Classes
+{
+    class SnippetGeneratorMembers
+    {
+     
+    }
+
+    //struct ClassGeneratorMembers
+    //{
+
+    //}
+
+    public enum FileStatusType
+    {
+        Notsaved = 0,
+        Saved = 1
+    }
+
+    public static class GeneratorConstants
+    {
+        public static string NewLine = "\n";
+        public static string Tab = "\t";
+        public static string NewlineTab = "\n\t";
+        public static string TabNewline = "\t\n";
+        public static string CarriageReturn = "\r";
+    }
+
+    public struct ABC
+    {
+
+    }
+    [Flags]
+    [Obsolete]
+    [Serializable]
+    public enum Days
+    {
+        Sun = 1,
+        Mon = 2,
+        Tue = 3,
+        Wed = 4,
+        Thu = 5,
+        Fri = 6,
+        Sat = 7
+    }
+
+}
+
+internal class MyClass
+{
+    protected internal static bool bChecked = false;
+}
+internal static class SClass
+{
+}
